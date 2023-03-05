a775f1e6b49700cc456412c85c1c90a0a8b1090b
krzysztofkn <krzysztof.kna@gmail.com>
2023-03-05 13:55:43 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 
 @SpringBootApplication
 @RestController
